fd45ec93b14944264c4278bb7a0a10c0e9c6081e
Ronit Katikaneni <ronitkat08@gmail.com>
2025-04-07 17:35:32 -0500

    2nd Update

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,9 @@
+<script>
+    // Import the environment variable
+    export const apiKey = import.meta.env.NASA_API_KEY;
+</script>
+
+
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<p>Here is my API Key: {apiKey}</p>
