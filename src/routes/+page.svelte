306b0d36df953bb9d78f2dade6b0c10166f4c955
Ronit Katikaneni <ronitkat08@gmail.com>
2025-04-07 17:38:49 -0500

    third change

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,8 +1,7 @@
 <script>
-    // Import the environment variable
-    export const apiKey = import.meta.env.NASA_API_KEY;
+    // Access the environment variable
+    export const apiKey = import.meta.env.VITE_NASA_API_KEY;
 </script>
-
 
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
